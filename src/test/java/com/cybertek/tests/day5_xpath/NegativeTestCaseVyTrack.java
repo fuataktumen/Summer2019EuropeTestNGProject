adc1f58f696dd4abeae887c2688c7a3a26167f44
Fuat <myEmail@Gmail.com>
2019-11-13 16:36:47 -0800

    vyTrack test case positive

---
+++ src/test/java/com/cybertek/tests/day5_xpath/NegativeTestCaseVyTrack.java
@@ -0,0 +1,34 @@
+package com.cybertek.tests.day5_xpath;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class NegativeTestCaseVyTrack {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://qa1.vytrack.com/user/login");
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys("user");
+        WebElement password = driver.findElement(By.id("prependedInput2"));
+        password.sendKeys("UserUser");
+        WebElement logİN = driver.findElement(By.id("_submit"));
+        logİN.click();
+
+        String expectedResult= "Invalid user name or password.";
+        WebElement messageElement = driver.findElement(By.xpath("//div[text()='Invalid user name or password.']"));
+        String actualResult=messageElement.getText();
+        if(expectedResult.equals(actualResult)) {
+            System.out.println("PASS");
+        }else {
+
+            System.out.println("FAIL");
+            System.out.println("actualResult = " + actualResult);
+            System.out.println("expectedResult = " + expectedResult);
+        }
+
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day5_xpath/PositiveTestCaseVyTrack.java
@@ -0,0 +1,33 @@
+package com.cybertek.tests.day5_xpath;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PositiveTestCaseVyTrack {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://qa1.vytrack.com/user/login");
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys("user10");
+        WebElement password = driver.findElement(By.id("prependedInput2"));
+        password.sendKeys("UserUser123");
+        WebElement logİN = driver.findElement(By.id("_submit"));
+        logİN.click();
+        String expectedURL = "https://qa1.vytrack.com/";
+        String actualResult = driver.getCurrentUrl();
+        if(expectedURL.equals(actualResult)){
+
+            System.out.println("PASS");
+        }else{
+
+            System.out.println("FAIL");
+            System.out.println("actualResult = " + actualResult);
+            System.out.println("expectedURL = " + expectedURL);
+        }
+    }
+}
